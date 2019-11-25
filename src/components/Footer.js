b6ddabd44834c6c49677a511488db8779959c095
prasetyo507 <prasetyo507@gmail.com>
2019-11-25 14:07:42 +0700

    menambah template MDBreact

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { MDBFooter } from "mdbreact";
+
+const Footer = () => {
+  return (
+    <MDBFooter color="blue" className="text-center font-small darken-2">
+      <p className="footer-copyright mb-0 py-3 text-center">
+        &copy; {new Date().getFullYear()} Copyright:{" "}
+        <a href="https://ubharajaya.ac.id">
+          {" "}
+          Direktorat Pengembangan Teknologi Informasi{" "}
+        </a>
+      </p>
+    </MDBFooter>
+  );
+};
+
+export default Footer;
---
+++ src/components/sideNavigation.js
@@ -0,0 +1,54 @@
+import React from "react";
+import logo from "../assets/mdb-react.png";
+import { MDBListGroup, MDBListGroupItem, MDBIcon } from "mdbreact";
+import { NavLink } from "react-router-dom";
+
+const TopNavigation = () => {
+  return (
+    <div className="sidebar-fixed position-fixed">
+      <a href="#!" className="logo-wrapper waves-effect">
+        <img alt="Sarpras UBJ logo" className="img-fluid" src={logo} />
+      </a>
+      <MDBListGroup className="list-group-flush">
+        <NavLink exact={true} to="/" activeClassName="activeClass">
+          <MDBListGroupItem>
+            <MDBIcon icon="chart-pie" className="mr-3" />
+            Dashboard
+          </MDBListGroupItem>
+        </NavLink>
+        <NavLink to="/pembelian" activeClassName="activeClass">
+          <MDBListGroupItem>
+            <MDBIcon icon="coins" className="mr-3" />
+            Pembelian
+          </MDBListGroupItem>
+        </NavLink>
+        <NavLink to="/pendataan" activeClassName="activeClass">
+          <MDBListGroupItem>
+            <MDBIcon icon="clipboard-list" className="mr-3" />
+            Pendataan
+          </MDBListGroupItem>
+        </NavLink>
+        <NavLink to="/perawatan" activeClassName="activeClass">
+          <MDBListGroupItem>
+            <MDBIcon icon="hammer" className="mr-3" />
+            Perawatan
+          </MDBListGroupItem>
+        </NavLink>
+        <NavLink to="/laporan" activeClassName="activeClass">
+          <MDBListGroupItem>
+            <MDBIcon icon="chart-pie" className="mr-3" />
+            Laporan
+          </MDBListGroupItem>
+        </NavLink>
+        <NavLink to="/404" activeClassName="activeClass">
+          <MDBListGroupItem>
+            <MDBIcon icon="exclamation" className="mr-3" />
+            404
+          </MDBListGroupItem>
+        </NavLink>
+      </MDBListGroup>
+    </div>
+  );
+};
+
+export default TopNavigation;
