32423562d33b19d865025abe3e82834055966a05
prasetyo507 <prasetyo507@gmail.com>
2019-11-26 10:41:09 +0700

    install template adminLTE

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
-import Routes from '../src/components/Routes';
-import TopNavigation from './components/topNavigation';
-import SideNavigation from './components/sideNavigation';
-import Footer from './components/Footer';
-import './index.css';
+import React, { Component } from "react";
+import Header from "./components/Header.js";
+import Menu from "./components/Menu.js";
+import Footer from "./components/Footer.js";
 
 class App extends Component {
-  
+  state = {};
   render() {
     return (
-        <div className="flexible-content">
-          <TopNavigation />
-          <SideNavigation />
-          <main id="content" className="p-5">
-            <Routes />
-          </main>
-          <Footer />
-        </div>
+      <div>
+        <Header />
+        <Menu />
+        <Footer />
+      </div>
     );
   }
 }
