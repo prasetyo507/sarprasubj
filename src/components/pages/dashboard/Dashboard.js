42333dc8773616b568358abdfd94ee34dd1a8fdb
prasetyo507 <prasetyo507@gmail.com>
2019-12-01 09:16:45 +0700

    buat service dengan axios

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react';
-import Section from '../../common/Section';
-import Master from '../Master';
+import React, { Component } from "react";
+import Section from "../../common/Section";
+import Master from "../Master";
+import Services from "../../services/Services";
 
 class Home extends Component {
-    render() {
-        return (
-            <Master>
-                <Section pageName={'Dashboard'} pageSubject={'Control panel'}>
-                    <h1>Hello, Kardinah indriana meutia!</h1>
-                </Section>
-            </Master>
-        )
-    }
+  componentDidMount() {
+    Services.get("users/1/albums")
+      .then(function(response) {
+        // handle success
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function() {
+        // always executed
+      });
+  }
+  render() {
+    return (
+      <Master>
+        <Section pageName={"Dashboard"} pageSubject={"Control panel"}>
+          <h1>Hello, Kardinah indriana meutia!</h1>
+        </Section>
+      </Master>
+    );
+  }
 }
 
 export default Home;
